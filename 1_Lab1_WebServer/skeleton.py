b2102753bcdaef7b858e0f2705ce9c9c2027571e
dengl11 <dengl11@stanford.edu>
2017-09-18 10:43:18 -0700

    -

--- 1_Lab1_WebServer/skeleton.py
+++ 1_Lab1_WebServer/skeleton.py
@@ -5,16 +5,21 @@
 from socket import *
 import sys # In order to terminate the program
 
+# SOCK_STREAM: TCP Socket 
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
 #Prepare a sever socket
 #Fill in start
-serverSocket.bind(('', 3000))
+serverSocket.bind(('', 3004))
+
+# must listen before accept 
 serverSocket.listen()
 #Fill in end
+
 quest = 0
+
 while True:
     #Establish the connection
-    print('Ready to serve...')
     #Fill in start
     connectionSocket, addr =  serverSocket.accept()
     #Fill in end
@@ -24,24 +29,42 @@ while True:
         #Fill in start
         message = connectionSocket.recv(1024).decode()
         #Fill in end
-        print("message: \n{}".format(message))
         filename = message.split()[1]
-        print("filename: {}".format(filename))
         f = open(filename[1:])
         #Fill in start
         outputdata = f.readlines()
         #Fill in end
-        # #Send one HTTP header line into socket
+
+        # Send one HTTP header line into socket
         #Fill in start
+        response_header = "\n".join([
+            "HTTP/1.1  ", 
+            "Content-Type: text/html",
+            "\r\n",
+            ]) 
+
+        # encode: string -> bytes 
+        connectionSocket.send(response_header.encode())
         #Fill in end
-        # #Send the content of the requested file to the client
+
+        # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
+
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
-    except IOError:
+    except IOError as e:
         #Send response message for file not found
         #Fill in start
+        response_msg = "\n".join([
+            "HTTP/1.1 404 Not Found", 
+            "Content-Type: text/html",
+            "\r\n",
+            "Your file is not found >_<"
+            ]) 
+
+        connectionSocket.send(response_msg.encode())
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
         #Close client socket
