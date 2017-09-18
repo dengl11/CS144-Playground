3a5a4a5573efd02f011eb72012e38a9af5e9562c
dengl11 <dengl11@stanford.edu>
2017-09-18 13:39:28 -0700

    -

--- 2_UDPPinger/server.py
+++ 2_UDPPinger/server.py
@@ -1,5 +1,6 @@
 
 # UDPPingerServer.py
+
 # We will need the following module to generate randomized lost packets
 import random
 from socket import *
@@ -15,10 +16,10 @@ while True:
 	rand = random.randint(0, 10)    
 	# Receive the client packet along with the address it is coming from 
 	message, address = serverSocket.recvfrom(1024)
-	# Capitalize the message from the client
-	message = message.upper()
 	# If rand is less is than 4, we consider the packet lost and do not respond
 	if rand < 4:
 		continue
 	# Otherwise, the server responds    
+	# Capitalize the message from the client
+	message = message.upper()
 	serverSocket.sendto(message, address)
